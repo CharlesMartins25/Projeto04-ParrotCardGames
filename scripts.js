87e5aaa6e42b16f1163e06dafeb447c65d4183eb
Charles <charles.marttins@gmail.com>
2022-04-11 17:26:28 -0300

    projeto, java incompleto

--- scripts.js
+++ scripts.js
@@ -1,8 +1,23 @@
+let numeroDeJogadas = [];
+let bloquearCarta;
+
+
+
 let numeroInicial = prompt("Insira o número de cartas que deseja jogar!");
 
 while (numeroInicial % 2 || numeroInicial < 4 || numeroInicial > 14 ) {
     numeroInicial = prompt("Insira um número válido (4 até 14)")
 }
+ let cartas = ["imagens/bobrossparrot.gif",
+ "imagens/bobrossparrot.gif",
+ "imagens/explodyparrot.gif",
+ "imagens/explodyparrot.gif",
+ "imagens/fiestaparrot.gif",
+ "imagens/fiestaparrot.gif",
+ "imagens/metalparrot.gif",
+
+
+]
 
 
 
