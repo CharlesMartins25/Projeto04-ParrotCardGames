ab05b39ac524a4811635863b5a0367b31d79f254
Charles <charles.marttins@gmail.com>
2022-04-11 13:30:03 -0300

    atribuir imagens as cartas

---
+++ scripts.js
@@ -0,0 +1,13 @@
+let numeroInicial = prompt("Insira o número de cartas que deseja jogar!");
+
+while (numeroInicial % 2 || numeroInicial < 4 || numeroInicial > 14 ) {
+    numeroInicial = prompt("Insira um número válido (4 até 14)")
+}
+
+
+
+function virarCarta (elemento){
+   
+
+    
+}
